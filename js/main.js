fdfe1f3a761f86b9b68847043d0df0c6a027ccfb
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2025-02-12 08:27:03 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+(
+  function() {
+    document.addEventListener("mousemove", parallax);
+    const element = document.getElementById("parallax");
+
+    function parallax(e) {
+      let width = window.innerWidth / 2;
+      let height = window.innerHeight / 2;
+
+      let mouse_x = e.clientX;
+      let mouse_y = e.clientY;
+
+      element.style.backgroundPosition = `${50 + (mouse_x - width) * 0.005}% ${50 + (mouse_y - height) * 0.005}%`;
+    }
+  }
+)();
+
