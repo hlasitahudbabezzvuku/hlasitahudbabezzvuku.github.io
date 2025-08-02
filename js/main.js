2619c0103b1f512af7010813680b69242b02c3b7
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2025-08-02 19:18:39 +0200

    New formating and test of commit signing

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 (
-  function() {
-    document.addEventListener("mousemove", parallax);
-    const element = document.getElementById("parallax");
+    function() {
+        document.addEventListener("mousemove", parallax);
+        const element = document.getElementById("parallax");
 
-    function parallax(e) {
-      let width = window.innerWidth / 2;
-      let height = window.innerHeight / 2;
+        function parallax(e) {
+            let width = window.innerWidth / 2;
+            let height = window.innerHeight / 2;
 
-      let mouse_x = e.clientX;
-      let mouse_y = e.clientY;
+            let mouse_x = e.clientX;
+            let mouse_y = e.clientY;
 
-      element.style.backgroundPosition = `${50 + (mouse_x - width) * 0.01}% ${50 + (mouse_y - height) * 0.02}%`;
+            element.style.backgroundPosition = `${50 + (mouse_x - width) * 0.01}% ${50 + (mouse_y - height) * 0.02}%`;
+        }
     }
-  }
 )();
 
